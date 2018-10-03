05f6e50ad58ec0da618e896c3e87357470fc8478
Rakhitha <rakhitharr@live.com>
2018-10-03 09:26:08 +0530

    Verification using decoding

--- src/main/java/ResourceFilter.java
+++ src/main/java/ResourceFilter.java
@@ -1,3 +1,5 @@
+import org.json.simple.JSONObject;
+//import org.json.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
@@ -5,10 +7,8 @@ import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
+import java.math.BigInteger;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.Charset;
@@ -25,6 +25,7 @@ public class ResourceFilter implements Filter {
     String client_id;
     String refreshToken;
     String client_secret;
+    Object obj;
 
     public void init(FilterConfig config) throws ServletException {
         fConfig = config;
@@ -44,57 +45,101 @@ public class ResourceFilter implements Filter {
 
             if("Bearer".equals(params[0])){
                 String access_Token = params[1];
-                //build url
-                QueryBuilder codeBuilder = new QueryBuilder();
-                codeBuilder.append("token", access_Token);
+                byte[] ba = Base64.getDecoder().decode(access_Token.split("\\.")[1]);
 
-                String EndPoint = " https://localhost:9443/oauth2/introspect";
-                String url = codeBuilder.returnQuery(EndPoint);
+                String decoded = new String(ba);
 
+                JSONParser parser = new JSONParser();
 
-                URL object = new URL(url);
-                HttpURLConnection con = (HttpURLConnection) object.openConnection();
+                try {
+                    obj = parser.parse(decoded);
 
-                con.setRequestMethod("POST");
-
-                //add request header
-                con.setRequestProperty("Content-type", "application/x-www-form-urlencoded");
-                con.setRequestProperty("Authorization", "Bearer " + access_Token);
-
-                try{
-                    BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-
-                    String inputLine;
-                    StringBuffer re = new StringBuffer();
-                    while ((inputLine = in.readLine()) != null) {
-                        re.append(inputLine);
-                    }
-                    in.close();
-
-                    //Read JSON response
-                    org.json.JSONObject myResponse = new org.json.JSONObject(re.toString());
-                    System.out.println(myResponse);
-
-
-                    String active = String.valueOf(myResponse.getBoolean("active"));
-                    String scope = (String)myResponse.get("scope");
-                    session.setAttribute("scope",scope);
-                    String[] scopes = scope.split(" ");
-//                    String client = (String)myResponse.get("client_id");
-//
-//                    String client_id = (String)session.getAttribute("client_id");
-
-                    if("true".equals(active) && Arrays.asList(scopes).contains("read")){
-                        chain.doFilter(request,response);
-                    }
-                    else{
-                        response.setContentType("application/json");
-                        response.setStatus(401);
-                    }
-
-                }
-                catch (IOException e){
+                } catch (ParseException e) {
                     System.out.println(e);
+                }
+
+                JSONObject jsonObj = (JSONObject) obj;
+                long epoch = System.currentTimeMillis()/1000;
+                long iat = (Long)jsonObj.get("iat");
+                long exp = (Long)jsonObj.get("exp");
+
+                boolean active = ((iat < epoch) && (exp >epoch));
+
+                String scope = (String)jsonObj.get("scope");
+                session.setAttribute("scope",scope);
+                System.out.println("Scopes: " + scope);
+                String[] scopes = scope.split(" ");
+
+                if((active) && Arrays.asList(scopes).contains("read")) {
+                    chain.doFilter(request, response);
+                }
+                else{
+                    response.setContentType("application/json");
+                    response.setStatus(401);
+                }
+                //build url
+//                QueryBuilder codeBuilder = new QueryBuilder();
+//                codeBuilder.append("token", access_Token);
+//
+////                String EndPoint = " https://localhost:9443/oauth2/introspect";
+//                String EndPoint = "http://localhost:8082/auth/realms/IFS/protocol/openid-connect/token/introspect";
+//                String url = EndPoint + "?";
+//                String credentials = (String)request.getHeader("ClientCredentials");
+//                codeBuilder.append("token", access_Token);
+//                String body = codeBuilder.returnQuery("");
+
+
+//                URL object = new URL(url);
+//                HttpURLConnection con = (HttpURLConnection) object.openConnection();
+//
+//                con.setRequestMethod("POST");
+//
+//                //add request header
+//                con.setRequestProperty("Content-type", "application/x-www-form-urlencoded");
+////                con.setRequestProperty("Authorization", "Bearer " + access_Token);
+//                con.setRequestProperty("Authorization", "Basic " + credentials);
+//
+//                con.setDoOutput(true);
+//                DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+//                wr.writeBytes(body);
+//                wr.flush();
+//                wr.close();
+
+
+//                try{
+//                    BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+//
+//                    String inputLine;
+//                    StringBuffer re = new StringBuffer();
+//                    while ((inputLine = in.readLine()) != null) {
+//                        re.append(inputLine);
+//                    }
+//                    in.close();
+//
+//                    //Read JSON response
+//                    org.json.JSONObject myResponse = new org.json.JSONObject(re.toString());
+//                    System.out.println(myResponse);
+//
+//
+//                    String active = String.valueOf(myResponse.getBoolean("active"));
+//                    String scope = (String)myResponse.get("scope");
+//                    session.setAttribute("scope",scope);
+//                    String[] scopes = scope.split(" ");
+////                    String client = (String)myResponse.get("client_id");
+////
+////                    String client_id = (String)session.getAttribute("client_id");
+//
+//                    if("true".equals(active) && Arrays.asList(scopes).contains("read")){
+//                        chain.doFilter(request,response);
+//                    }
+//                    else{
+//                        response.setContentType("application/json");
+//                        response.setStatus(401);
+//                    }
+//
+//                }
+//                catch (IOException e){
+//                    System.out.println(e);
 //                if (session != null) {
 //                    session.invalidate();
 //                }
@@ -106,7 +151,7 @@ public class ResourceFilter implements Filter {
 //                    response.sendRedirect("home?errorMessage=Access Token Invalid");
 //                }
 
-                }
+//                }
             }
             else{
                 String base64Credentials = authString.substring("Basic".length()).trim();
